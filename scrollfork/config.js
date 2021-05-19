ecbd266270e33f2241666e30ee8171769c416843
Hollie Eggleton <hollie.eggleton987@gmail.com>
2021-05-19 18:54:40 -0400

    yes

--- scrollfork/config.js
+++ scrollfork/config.js
@@ -21,7 +21,7 @@ var followPitch = 0;
 var config = {
     style: 'mapbox://styles/mapbox/outdoors-v11',
     accessToken: 'pk.eyJ1IjoiaG9semVnZyIsImEiOiJja292eXAzbnUwMHdhMnVsdTM4c2MyOTF3In0.6RiKfls_ZXfiI0k-q7gLDQ',
-    showMarkers: false,
+    showMarkers: true,
     theme: 'dark',
     alignment: 'left',
     title: 'Liberty Project',
