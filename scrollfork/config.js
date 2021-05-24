aea297a1c1d7f1b1683e52993b740812a94b174a
Hollie Eggleton <hollie.eggleton987@gmail.com>
2021-05-24 16:09:50 -0400

    xdva

--- scrollfork/config.js
+++ scrollfork/config.js
@@ -114,14 +114,14 @@ var config = {
         // You can add a normal slide in between the other slides
         {
             id: 'slide-4',
-            title: 'Second Title',
+            title: 'i AM A STATIONARY cHAPTER. ',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                // center: [-84.748093, 47.472679],
-                // zoom: 9.83,
-                // pitch: 0.00,
-                // bearing: 0.00 
+                center: [-84.748093, 47.472679],
+                zoom: 9.83,
+                pitch: 0.00,
+                bearing: 0.00 
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
