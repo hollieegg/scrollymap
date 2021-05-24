0b1377f206ba07bd70ca8787c1f4c21b67844a78
Hollie Eggleton <hollie.eggleton987@gmail.com>
2021-05-24 15:24:31 -0400

    y

--- scrollfork/config.js
+++ scrollfork/config.js
@@ -92,7 +92,7 @@ var config = {
                 center: [-84.748093, 47.472679],
                 zoom: 15.00,
                 pitch: 0.00,
-                bearing: 0.00 
+                bearing: 15.00 
             },
             onChapterEnter: [],
             onChapterExit: []
