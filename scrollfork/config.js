d0297c99d3a193dd42f6fc623399341664c67f7d
Hollie Eggleton <hollie.eggleton987@gmail.com>
2021-05-24 16:24:22 -0400

    d

--- scrollfork/config.js
+++ scrollfork/config.js
@@ -116,7 +116,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-84.748093, 47.472679],
+                // center: [-84.748093, 47.472679],
                 // zoom: 9.83,
                 // pitch: 0.00,
                 // bearing: 0.00 
