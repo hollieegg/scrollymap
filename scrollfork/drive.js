f1e649d61da44161ab1130494309950bf29655d0
Hollie Eggleton <hollie.eggleton987@gmail.com>
2021-05-31 15:06:55 -0400

    line

--- scrollfork/drive.js
+++ scrollfork/drive.js
@@ -32,7 +32,7 @@ function createLine() {
     // const segments = lineDistance / rects;
     // const segments = lineDistance / driveSlides / rects;
     // const segments = lineDistance / driveSlides;
-    const segments =  driveSlides;
+    const segments =  line / 13;
 
 
     // what units do you want to use?
