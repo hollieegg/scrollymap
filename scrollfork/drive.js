945128a00bb6b82c1b0eab4eba55317466be52c6
Hollie Eggleton <hollie.eggleton987@gmail.com>
2021-05-31 17:40:13 -0400

    dg

--- scrollfork/drive.js
+++ scrollfork/drive.js
@@ -15,15 +15,19 @@ var geojsonPoint = {
 
 function createLine() {
 
-    // get the coordinates of the line you want to highlight
-    let extentArray = routeData.features[0].geometry.coordinates;
+    // // get the coordinates of the line you want to highlight
+    // let extentArray = routeData.features[0].geometry.coordinates;
     
 
-    // create a turf linestring based on the line coordinates
-    const line = turf.lineString(extentArray);
+    // // create a turf linestring based on the line coordinates
+    // const line = turf.lineString(extentArray);
 
-    // calculate the total length of the line
-    const lineDistance = turf.lineDistance(line);
+    // // calculate the total length of the line
+    // const lineDistance = turf.lineDistance(line);
+
+    var line = turf.lineString([[-83, 30], [-84, 36], [-78, 41]]);
+    var options = {units: 'miles'};
+    const along = turf.along(line, 200, options);
 
     // how many points you want along the path (more = smoother animation)
     // const rects = driveTime;
